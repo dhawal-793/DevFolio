456533e1d0edad96bb9e67625d3fc9fbb4fda323
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-12 22:03:40 +0530

    Addded some animation styles and swapped the icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import SectionHeading from './SectionHeading'
-import { BsPersonFill, HiOutlineMail, AiOutlineMessage, FaTelegramPlane } from "../data/icons"
+import { Person, GMail, Chat, Submit } from './Icons'
 
 const Contact = () => {
     const initialFormData = { name: "", email: "", message: "" };
@@ -26,20 +26,23 @@ const Contact = () => {
                         className='flex gap-3 flex-col w-full md:w-1/2 '
                     >
                         <div className="relative w-full">
-                            <input type="text" name="name" placeholder="Enter Your Name" className='form-input' value={formData.name} onChange={handleChange} />
-                            <BsPersonFill size={20} className="text-white absolute left-3 top-1/2 -translate-y-1/2" />
+                            <input type="text" name="name" placeholder="Enter Your Name" className='peer form-input' value={formData.name} onChange={handleChange} />
+                            <span className="peer-placeholder-shown:grayscale peer-focus:grayscale-0 peer-active:grayscale-0 absolute left-3 top-1/2 -translate-y-1/2"><Person /></span>
                         </div>
                         <div className="relative w-full">
-                            <input type="email" name="email" placeholder="Enter Your Email" className='form-input' value={formData.email} onChange={handleChange} />
-                            <HiOutlineMail size={20} className="text-white absolute left-3 top-1/2 -translate-y-1/2" />
+                            <input type="email" name="email" placeholder="Enter Your Email" className='peer form-input' value={formData.email} onChange={handleChange} />
+                            <span className="peer-placeholder-shown:grayscale peer-focus:grayscale-0 peer-active:grayscale-0 absolute left-3 top-1/2 -translate-y-1/2"><GMail /></span>
                         </div>
                         <div className="relative w-full">
                             <textarea name="message" placeholder='Your Meaasge' rows="10" cols="30"
-                                className="form-input" value={formData.message} onChange={handleChange}
-                            /><AiOutlineMessage size={20} className=" text-white absolute left-3 top-[0.8rem] " />
+                                className="form-input peer" value={formData.message} onChange={handleChange} />
+                            <span className="peer-placeholder-shown:grayscale peer-focus:grayscale-0 peer-active:grayscale-0 absolute left-3 top-[0.8rem] "><Chat /></span>
                         </div>
 
-                        <button className='hover:text-blue-800 flex items-center justify-center mr-auto text-white bg-gradient-to-b from-cyan-500 to-blue-500 rounded-md font-semibold px-12 py-2 hover:scale-[1.02] duration-500'>Let's Connect <FaTelegramPlane size={28} className="pl-3 group-hover:scale-110 " /></button>
+                        <button className='hover:text-blue-800 flex items-center justify-center mr-auto group text-white bg-gradient-to-b from-cyan-500 to-blue-500 rounded-md font-semibold px-12 py-2 hover:scale-[1.02] duration-500'>Let's Connect
+                            <span className="scale-0 -translate-x-10 origin-left group-hover:scale-100 group-hover:translate-x-3 duration-300 transition-all delay-200 ease-in-out"> <Submit />
+                            </span>
+                        </button>
                     </form>
                 </div>
             </div>
