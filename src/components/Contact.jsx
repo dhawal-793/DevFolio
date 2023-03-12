26115854063efd1a4c5bdb5513621b97ab9921a5
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-12 18:06:49 +0530

    Added states for input elements and fixed the form reset after redirection using useeffect

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SectionHeading from './SectionHeading'
 import { BsPersonFill, HiOutlineMail, AiOutlineMessage, FaTelegramPlane } from "../data/icons"
 
 const Contact = () => {
+    const initialFormData = { name: "", email: "", message: "" };
+    const [formData, setFormData] = useState(initialFormData)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value })
+    }
+
+    useEffect(() => {
+        setFormData(initialFormData)
+    }, [])
+
     return (
         <div name="Contact" className='w-full h-screen bg-gradient-to-b to-gray-800 from-black'>
             <div className='section pb-8'>
@@ -11,18 +23,19 @@ const Contact = () => {
                     <form
                         action="https://getform.io/f/b6e07c9a-c5f3-4d99-8c12-753b18021208"
                         method='POST'
-                        className='flex gap-3 flex-col w-full md:w-1/2 '>
+                        className='flex gap-3 flex-col w-full md:w-1/2 '
+                    >
                         <div className="relative w-full">
-                            <input type="text" name="name" placeholder="Enter Your Name" className='form-input' />
+                            <input type="text" name="name" placeholder="Enter Your Name" className='form-input' value={formData.name} onChange={handleChange} />
                             <BsPersonFill size={20} className="text-white absolute left-3 top-1/2 -translate-y-1/2" />
                         </div>
                         <div className="relative w-full">
-                            <input type="email" name="email" placeholder="Enter Your Email" className='form-input' />
+                            <input type="email" name="email" placeholder="Enter Your Email" className='form-input' value={formData.email} onChange={handleChange} />
                             <HiOutlineMail size={20} className="text-white absolute left-3 top-1/2 -translate-y-1/2" />
                         </div>
                         <div className="relative w-full">
                             <textarea name="message" placeholder='Your Meaasge' rows="10" cols="30"
-                                className="form-input"
+                                className="form-input" value={formData.message} onChange={handleChange}
                             /><AiOutlineMessage size={20} className=" text-white absolute left-3 top-[0.8rem] " />
                         </div>
 
