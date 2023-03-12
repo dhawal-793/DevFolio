e8747dcb380756e69bb50370e41d04b84c68f3a4
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-12 21:06:10 +0530

    Removed Unnecessary Code and removed react icons from contactInfo

--- src/data/contactInfo.jsx
+++ src/data/contactInfo.jsx
@@ -1,55 +1,49 @@
-
-import { FaLinkedin, FaGithub, FaTelegramPlane } from "react-icons/fa"
-import { HiOutlineMail } from "react-icons/hi";
-// import { FaDiscord, FaTwitter } from "react-icons/fa"
-import { BsFillPersonLinesFill } from "react-icons/bs";
-
-import LinkedIn, { GitHub, GMail, Telegram ,Resume} from "../components/Icons";
+import LinkedIn, { GitHub, GMail, Telegram, Person } from "../components/Icons";
 // import {  Discord, Twitter } from "../components/Icons";
 const contactInfo = [
     {
         id: 1,
         name: "LinkedIn",
         link: "https://www.linkedin.com/in/dhawal793/",
-        icons: [<LinkedIn />, <FaLinkedin size={17} color="#0078d4" />]
+        icon: <LinkedIn />,
     },
     {
         id: 2,
         name: "GitHub",
         link: "https://github.com/dhawal-793",
-        icons: [<GitHub />, <FaGithub size={17} color="#fff" />]
+        icon: <GitHub />,
     },
     {
         id: 3,
         name: "Mail",
         link: "mailto:vijaydhawal9001@gmail.com",
-        icons: [<GMail />, <HiOutlineMail size={17} color="#e53935" />]
+        icon: <GMail />,
     },
 
     {
         id: 4,
         name: "Telegram",
         link: "https://t.me/DV_793",
-        icons: [<Telegram />, <FaTelegramPlane size={17} color="#03A9F4" />]
+        icon: <Telegram />,
     },
     {
         id: 5,
         name: "Resume",
         link: "/resume.pdf",
-        icons: [<Resume/> ,<BsFillPersonLinesFill size={17} color="#5EEAC3" />],
+        icon: <Person />,
         download: true
     }
     // {
     //   id: 6,
     //   name: "Twitter",
     //   link: "twitter.com",
-    //   icons: [<Twitter />, <FaTwitter size={17} color="#5EEAC3" />]
+    //   icon: <Twitter />,
     // },
     // {
     //   id: 7,
     //   name: "Discord",
     //   link: "discord.com",
-    //   icons: [<Discord />, <FaDiscord size={17} color="#5EEAC3" />]
+    //   icon: <Discord />,
     // },
 ];
 
