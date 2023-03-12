fc78c2ffcf614af0a9c116ebe2e67bb0df5372fb
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-12 19:20:15 +0530

    Updated contactInfo component to use icons component and added more contact details

--- src/data/contactInfo.jsx
+++ src/data/contactInfo.jsx
@@ -1,45 +1,56 @@
-import { FaLinkedin, FaGithub, HiOutlineMail, FaTelegramPlane, BsPersonLinesFill } from "./icons";
 
+import { FaLinkedin, FaGithub, FaTelegramPlane } from "react-icons/fa"
+import { HiOutlineMail } from "react-icons/hi";
+// import { FaDiscord, FaTwitter } from "react-icons/fa"
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
+import LinkedIn, { GitHub, GMail, Telegram ,Resume} from "../components/Icons";
+// import {  Discord, Twitter } from "../components/Icons";
 const contactInfo = [
     {
         id: 1,
         name: "LinkedIn",
-        link: "https://www.linkedin.com/in/dhawal-vijayvargiya-188683214/",
-        icon: <FaLinkedin size={17} />,
-        style: "hover:text-white hover:bg-[#0A66C2]"
+        link: "https://www.linkedin.com/in/dhawal793/",
+        icons: [<LinkedIn />, <FaLinkedin size={17} color="#0078d4" />]
     },
     {
         id: 2,
         name: "GitHub",
         link: "https://github.com/dhawal-793",
-        icon: <FaGithub size={17} />,
-        style: "hover:text-[#E6ECF1] hover:bg-black"
-
+        icons: [<GitHub />, <FaGithub size={17} color="#fff" />]
     },
     {
         id: 3,
         name: "Mail",
         link: "mailto:vijaydhawal9001@gmail.com",
-        icon: <HiOutlineMail size={17} />,
-        style: "hover:bg-white hover:text-[#EB4334]"
-
+        icons: [<GMail />, <HiOutlineMail size={17} color="#e53935" />]
     },
+
     {
         id: 4,
         name: "Telegram",
         link: "https://t.me/DV_793",
-        icon: <FaTelegramPlane size={17} />,
-        style: "hover:text-white hover:bg-[#2FA2D8] "
-
+        icons: [<Telegram />, <FaTelegramPlane size={17} color="#03A9F4" />]
     },
     {
         id: 5,
         name: "Resume",
         link: "/resume.pdf",
-        icon: <BsPersonLinesFill size={17} />,
-        style: "hover:bg-black hover:text-[#5EEAC5]",
+        icons: [<Resume/> ,<BsFillPersonLinesFill size={17} color="#5EEAC3" />],
         download: true
     }
+    // {
+    //   id: 6,
+    //   name: "Twitter",
+    //   link: "twitter.com",
+    //   icons: [<Twitter />, <FaTwitter size={17} color="#5EEAC3" />]
+    // },
+    // {
+    //   id: 7,
+    //   name: "Discord",
+    //   link: "discord.com",
+    //   icons: [<Discord />, <FaDiscord size={17} color="#5EEAC3" />]
+    // },
 ];
 
 export default contactInfo;
