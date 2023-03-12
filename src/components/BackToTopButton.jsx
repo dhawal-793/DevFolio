762229f6bf472b1678f6eb4187f89a4441353c96
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-12 21:19:13 +0530

    Updated Backtotop component to use the new icons

--- src/components/BackToTopButton.jsx
+++ src/components/BackToTopButton.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { MdKeyboardArrowUp } from "../data/icons"
+import { ArrowUp } from "./Icons"
 import { Link } from 'react-scroll'
 
 const BackToTopButton = () => {
@@ -24,8 +24,7 @@ const BackToTopButton = () => {
             {btnVisiblity &&
                 <Link onClick={handleScroll} to="Home" smooth duration={500} className='' >
                     <div className=' z-[60] fixed bottom-4 right-4 w-8 h-8 flex justify-center items-center rounded-full bg-gradient-to-br from-blue-500 to-cyan-500 cursor-pointer hover:scale-105 duration-500 text-white'>
-                        <MdKeyboardArrowUp size={22} />
-
+                        <ArrowUp />
                     </div>
                 </Link>
             }
