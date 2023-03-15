dbc6666be59bd99b04bb2fa0045ec5434312aa81
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-15 21:06:18 +0530

    Created a constant for Techstacks

---
+++ src/data/techStack.jsx
@@ -0,0 +1,53 @@
+import {
+    SiJavascript, SiReact, SiNextdotjs, SiTailwindcss, SiGit, SiGithub, SiVisualstudiocode,
+
+} from "react-icons/si"
+
+
+const TechStack = [
+    {
+        id: 1,
+        icon: <SiReact size={22} className="text-[#00c8ff] z-20" />,
+        name: "React",
+
+    },
+    {
+        id: 2,
+        icon: <SiJavascript size={22} className="text-[#ffc800] z-20" />,
+        name: "Javascript",
+
+    },
+    {
+        id: 3,
+        icon: <SiTailwindcss size={22} className="text-[#0099ff] z-20" />,
+        name: "TailwindCSS",
+
+    },
+    {
+        id: 4,
+        icon: <SiNextdotjs size={22} className="text-[#c1b7b7] z-20" />,
+        name: "NextJs",
+
+    },
+    {
+        id: 5,
+        icon: <SiGit size={22} className="text-[#ff2f00] z-20" />,
+        name: "Git",
+
+    },
+    {
+        id: 6,
+        icon: <SiGithub size={22} className="text-[#bdbdbd] z-20" />,
+        name: "GitHub",
+
+    },
+    {
+        id: 7,
+        icon: <SiVisualstudiocode size={22} className="text-[#0099ff] z-20" />,
+        name: "VsCode",
+
+    },
+]
+
+export default TechStack;
+
