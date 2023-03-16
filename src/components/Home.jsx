f02772ecebf0f5ea5eb0ef7d92a6190b83d43fbc
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-16 11:43:32 +0530

    Added social Links, animation to button and replaced icon, updated margin padding and viewports

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation';
 import { Link } from 'react-scroll';
-import { MdKeyboardArrowRight } from "../data/icons";
+import { ArrowDown } from "./Icons";
 import Avatar from "../assets/Avatars/Avatar.svg"
+import contactInfo from '../data/contactInfo';
 const Home = () => {
-    const work = ['Frontend Development', 2000, 'Backend Development',2000,'Android Development',2000, 'Web Designing', 2000, 'UI/UX Designing', 2000]
+    const work = ['Frontend Development', 2000, 'Backend Development', 2000, 'Android Development', 2000, 'Web Designing', 2000, 'UI/UX Designing', 2000]
 
     return (
-        <div name='Home' className=' outline-blue-500 h-screen min-h-[80vh] w-full bg-gradient-to-b from-black via-black to-gray-800 '>
-            <div className='section gap-28 md:gap-1 md:justify-between items-center md:flex-row'>
-
+        <div name='Home' className='pt-8 h-full min-h-screen w-full flex items-center bg-gradient-to-b from-black via-black to-gray-800 '>
+            <div className='section gap-16 md:gap-1 md:justify-between items-center md:flex-row-reverse'>
+                <div className='flex flex-col justify-center md:w-2/5 max-w-[320px]'>
+                    <img src={Avatar} alt="" className=' rounded-full mx-auto w-full ' />
+                </div>
                 <div className='flex flex-col gap-3 justify-center text-gray-200 w-max md:w-3/5'>
                     <h2 className=' text-6xl lg:text-7xl font-bold '>Hi There,</h2>
                     <h2 className=' text-6xl lg:text-7xl font-bold '>I'm <span className='text-transparent bg-gradient-to-br from-cyan-300 to-blue-600 bg-clip-text'> Dhawal </span></h2>
                     <p className='text-gray-400 text-2xl font-semibold py-4 min-w-[25rem] max-w-[26rem]'>
-                        I am into 
+                        I am into
 
                         <span className='text-cyan-400 pl-2'>
                             <TypeAnimation
@@ -26,18 +29,28 @@ const Home = () => {
                             />
                         </span>
                     </p>
-                    <div>
-                        <Link to="About" smooth duration={500} className='group bg-gradient-to-br from-cyan-400 to-blue-600 flex items-center px-6 py-3 pl-8 rounded-lg ease-in duration-500 cursor-pointer w-fit'>
-                            About Me
-                            <span className="group-hover:rotate-90 duration-500 ml-2">
-                                <MdKeyboardArrowRight size={25} />
+                    <ul className="flex items-center z-40 gap-3 md:gap-8 lg:gap-4 justify-start ">
+                        {contactInfo.map(({ id, link, name, icon, download }) => {
+                            return (
+                                <li key={id} link={link} className={`group relative w-8 h-8 flex justify-center items-center rounded-full p-1 sm:grayscale  cursor-pointer duration-[450ms] transition ease-in hover:grayscale-0 hover:scale-105 focus:grayscale-0 focus:scale-105 active:grayscale-0 active:scale-105`}>
+                                    <a href={link} download={download} target="_blank" rel='noferrer'>{icon}</a>
+                                    <div className="flex scale-0 sm:group-hover:scale-100 group-focus:scale-100 transition ease-out duration-300 delay-100 origin-center justify-center items-center absolute z-50 -bottom-10 w-auto h-6 py-2 px-3 bg-gray-900 text-sm text-gray-300">
+                                        {name}
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <div className='py-5'>
+                        <Link to="About" smooth duration={500} className='z-30 group bg-gradient-to-br from-cyan-400 to-blue-600 flex items-center justify-center gap-3 px-6 py-3  rounded-lg ease-linear duration-300 delay-75 transition-all cursor-pointer w-40 relative '>
+                            <span className='pr-6 sm:pr-0 sm:group-hover:-translate-x-5 sm:transition-transform delay-[400ms] ease-out'>About Me</span>
+                            <span className="absolute z-30 right-5 sm:-translate-y-7 sm:group-hover:translate-y-0 sm:scale-0 transition-all group-hover:scale-125 ease-in delay-200 animate-pulse duration-500 ">
+                                <ArrowDown />
                             </span>
                         </Link>
                     </div>
                 </div>
-                <div className='flex flex-col justify-center md:w-2/5 max-w-[320px]'>
-                    <img src={Avatar} alt="" className=' rounded-full mx-auto w-full ' />
-                </div>
+
             </div>
         </div>
     )
