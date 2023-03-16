756428e227e0b8ab43ef9b5ce0f0d24f4d6eb007
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-16 11:15:33 +0530

    Updated the Arrow Right icon and changed name to ArrowDown

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -169,10 +169,10 @@ function ArrowRightLong() {
         </svg>
     )
 }
-function ArrowRight() {
+function ArrowDown() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" >
-            <path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#fff">
+            <path d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z"/>
         </svg>
     )
 }
@@ -196,4 +196,4 @@ function Download() {
 
 
 export default LinkedIn;
-export { GMail, GitHub, Telegram, Person, Discord, Twitter, Resume, Portfolio, Map, CopyRight, Code, Link, Chat, Submit, ArrowRightLong, ArrowRight, ArrowUp, Download };
+export { GMail, GitHub, Telegram, Person, Discord, Twitter, Resume, Portfolio, Map, CopyRight, Code, Link, Chat, Submit, ArrowRightLong, ArrowDown, ArrowUp, Download };
