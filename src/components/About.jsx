0924798cbbc881de48516bc745e7fa3de82ca053
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-16 12:20:22 +0530

    Updated height for different devices for responsiveness.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ import techStack from '../data/techStack';
 
 const About = () => {
     return (
-        <div name='About' className=' outline-blue-500 h-auto min-h-[90vh] w-full bg-gradient-to-b to-black via-black from-gray-800 '>
+        <div name='About' className=' pt-8 h-full min-h-screen w-full flex items-center  bg-gradient-to-b to-black via-black from-gray-800 '>
             <div className='py-20 section justify-between'>
                 <SectionHeading heading="About Me" />
                 <div className='mb-10  flex flex-col justify-center '>
@@ -17,13 +17,13 @@ const About = () => {
                 </div>
                 <div className='flex flex-col justify-center w-full px-10 md:px-20 lg:px-28 text-white'>
                     <h1 className=' text-4xl md:text-5xl lg:text-6xl font-semibold '>Hi, I'm <span className='text-cyan-500'>Dhawal</span></h1>
-                    <div className='text-gray-400 py-4 space-y-3 md:space-y-2 md:pt-8 lg:pt-12 font-semibold text-justify'>
-                        <p>
-                            An aspiring Fullstack Web Developer mostly working with Frontend with React and Tailwind CSS.
-                            I also love to make beautiful Web Applications with technologies like MERN Stack, NextJs.
+                    <div className='text-gray-400 py-4 space-y-2 md:space-y-2 md:pt-8 lg:pt-12 font-semibold text-justify'>
+                        <p className='leading-tight'>
+                            An aspiring Fullstack Web Developer based in India. I mostly works on Frontend using React and Tailwind CSS.
                         </p>
-                        <p>
-                            Coding Enthusuast and Passionate Web Developer currently pursuing Bachelor's degree in computer Applications.
+                        <p className='leading-tight'> I also love to make beautiful Web Applications with technologies like MERN Stack, NextJs.</p>
+                        <p className='leading-tight'>
+                            Coding Enthusuast, quick learner and Passionate Web Developer currently pursuing Bachelor's degree in computer Applications at Mohanlal Sukhadia University, Udaipur, (Rajasthan).
                         </p>
                     </div>
                     <div className='py-3 text-gray-50 sm:flex font-semibold'>
@@ -47,7 +47,7 @@ const About = () => {
                     </div>
                     <div className='py-5'>
                         <a href={resume} target="_blank" rel="noreferrer" download={true} className='group bg-gradient-to-br from-cyan-400 to-blue-600 flex items-center justify-center gap-3 px-6 py-3  rounded-lg ease-linear duration-300 delay-75 transition-all cursor-pointer w-36 relative'>
-                            <span className='pr-6 sm:pr-0 sm:group-hover:-translate-x-3 sm:transition-transform delay-[400ms] ease-out'>Resume</span>
+                            <span className='pr-6 sm:pr-0 sm:group-hover:-translate-x-5 sm:transition-transform delay-[400ms] ease-out'>Resume</span>
                             <span className="absolute right-5 sm:scale-0 transition-all group-hover:scale-100 ease-in delay-200 animate-pulse duration-500 ">
                                 <Download/>
                             </span>
