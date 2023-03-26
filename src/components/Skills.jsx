c6799bb1362890ad799c1c4b3a285abd9d947c15
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-26 09:36:07 +0530

    Updated padding

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,8 +9,7 @@ const Skills = () => {
         <div name="Skills" className='pt-10 h-full min-h-screen w-full flex items-center bg-gradient-to-b from-gray-800 to-black'>
             <div className="section">
                 <SectionHeading heading="Skills" secondHeading="These are the Technologies I've worked with" />
-                <div className="p-4 rounded-lg bg-gray-900">
-
+                <div className="py-2 xs:p-4 rounded-lg bg-gray-900">
                     <GridLayout style="grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
                         {skills.map((skill, index) => {
                             return (
