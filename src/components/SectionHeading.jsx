6c7671c272db3b5e1f57ae4564d80c68a3d9800c
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-29 19:13:47 +0530

    Updated sectio heading component to use h2.

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SectionHeading = ({ heading,secondHeading }) => {
     return (
         <div className="pb-8 text-white">
-            <h3 className='text-4xl px-1 inline border-b-4 border-gray-400  font-bold '>{heading}</h3>
+            <h2 className='text-4xl px-1 inline border-b-4 border-gray-400  font-bold '>{heading}</h2>
             {secondHeading && <p className='py-6 text-lg'>
                 {secondHeading}
             </p>}
