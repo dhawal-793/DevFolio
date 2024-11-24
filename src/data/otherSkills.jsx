9c6961236a88ccaa8506af256abbc9be85472926
Dhawal Vijayvargiya <dhawal@gkmit.co>
2024-11-24 19:00:35 +0530

    Created seperate file for otherskills.

---
+++ src/data/otherSkills.jsx
@@ -0,0 +1,127 @@
+import {
+    Bootstrap,
+    C,
+    Canva,
+    CPlusPlus,
+    Java,
+    JQuery,
+    Kotlin,
+    MaterialUI,
+    CSharp,
+    DotNet,
+    SASS,
+    MERN,
+  } from "../components/SkilIcons";
+  
+  const skills = [
+    {
+      icon: <Bootstrap />,
+      name: "Bootstrap",
+      style: {
+        shadow: "shadow-[#6610F2]",
+        cover: "from-black to-[#6610F2] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <SASS />,
+      name: "SASS",
+      style: {
+        shadow: "shadow-[#CC6699]",
+        cover: "from-black to-[#CC6699] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <MaterialUI />,
+      name: "MaterialUI",
+      style: {
+        shadow: "shadow-[#1266A9]",
+        cover: "from-black to-[#1266A9] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <CPlusPlus />,
+      name: "C++",
+      style: {
+        shadow: "shadow-[#004482]",
+        cover: "from-black to-[#004482] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <Java />,
+      name: "Java",
+      style: {
+        shadow: "shadow-[#E76F00]",
+        cover: "from-black to-[#E76F00] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <C />,
+      name: "C",
+      style: {
+        shadow: "shadow-[#3949AB]",
+        cover: "from-black to-[#3949AB] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <Kotlin />,
+      name: "Kotlin",
+      style: {
+        shadow: "shadow-[#B14F97]",
+        cover: "from-black to-[#B14F97] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <CSharp />,
+      name: "C#",
+      style: {
+        shadow: "shadow-[#A179DC]",
+        cover: "from-black to-[#A179DC] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <JQuery />,
+      name: "JQuery",
+      style: {
+        shadow: "shadow-[#1266A9]",
+        cover: "from-black to-[#1266A9] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <Canva />,
+      name: "Canva",
+      style: {
+        shadow: "shadow-[#430098]",
+        cover: "from-black to-[#430098] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <DotNet />,
+      name: "DotNet Core",
+      style: {
+        shadow: "shadow-[#430098]",
+        cover: "from-black to-[#430098] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+    {
+      icon: <MERN />,
+      name: "MERN",
+      style: {
+        shadow: "shadow-[#549DD0]",
+        cover: "from-black to-[#326DE6] rounded-b-md",
+        opacity: "opacity-80",
+      },
+    },
+  ];
+  export default skills;
+  
